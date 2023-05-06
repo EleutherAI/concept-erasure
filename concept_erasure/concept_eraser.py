54366e33cd8df3fa7c11412b1d051a1c60750fe1
Nora Belrose <belrose.nora@gmail.com>
2023-05-06 06:49:34 +0000

    Move code over from elk repo

---
+++ concept_erasure/concept_eraser.py
@@ -0,0 +1,108 @@
+import torch
+from torch import Tensor, nn
+
+
+class ConceptEraser(nn.Module):
+    """Minimally edits features to prevent linear adversaries from detecting a concept."""
+
+    mean_x: Tensor
+    """Running mean of X."""
+
+    mean_y: Tensor
+    """Running mean of Y."""
+
+    u: Tensor
+    """Orthonormal basis of the subspace to remove."""
+
+    xcov_M2: Tensor
+    """Unnormalized cross-covariance matrix X^T Y."""
+
+    n: Tensor
+    """Number of samples seen so far."""
+
+    def __init__(
+        self,
+        x_dim: int,
+        y_dim: int,
+        *,
+        device: str | torch.device | None = None,
+        dtype: torch.dtype | None = None,
+        rank: int | None = None,
+    ):
+        super().__init__()
+
+        self.y_dim = y_dim
+        self.x_dim = x_dim
+        self.rank = rank or y_dim
+
+        self.register_buffer(
+            "mean_x", torch.zeros(x_dim, device=device, dtype=dtype)
+        )
+        self.register_buffer("mean_y", self.mean_x.new_zeros(y_dim))
+        self.register_buffer("u", self.mean_x.new_zeros(x_dim, self.rank))
+        self.register_buffer(
+            "xcov_M2",
+            self.mean_x.new_zeros(x_dim, y_dim),
+        )
+        self.register_buffer("n", torch.tensor(0, device=device, dtype=dtype))
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Remove the subspace responsible for correlations between x and y."""
+        *_, d, _ = self.xcov_M2.shape
+        assert self.n > 0, "Call update() before forward()"
+        assert x.shape[-1] == d
+
+        # First center the input
+        x_ = x - self.mean_x
+
+        # Remove the subspace. We treat x_ as a batch of (1 x d) vectors
+        proj = (x_[..., None, :] @ self.u) @ self.u.mT
+        x_ -= proj.squeeze(-2)
+
+        return x_
+
+    @torch.no_grad()
+    def update(self, x: Tensor, y: Tensor) -> "ConceptEraser":
+        """Update the running statistics with a new batch of data."""
+        *_, d, c = self.xcov_M2.shape
+
+        x = x.reshape(-1, d).type_as(self.mean_x)
+
+        n, *_, d2 = x.shape
+        assert d == d2, f"Unexpected number of features {d2}"
+
+        # y might start out 1D, but we want to treat it as 2D
+        y = y.reshape(n, -1).type_as(x)
+        assert y.shape[-1] == c, f"Unexpected number of classes {y.shape[-1]}"
+
+        self.n += n
+
+        # Welford's online algorithm
+        delta_x = x - self.mean_x
+        self.mean_x += delta_x.sum(dim=0) / self.n
+
+        delta_y = y - self.mean_y
+        self.mean_y += delta_y.sum(dim=0) / self.n
+        delta_y2 = y - self.mean_y
+
+        self.xcov_M2 += torch.einsum("b...m,b...n->...mn", delta_x, delta_y2)
+        if self.y_dim == self.rank:
+            # When we're entirely erasing the subspace, we can use QR instead of SVD to
+            # get an orthonormal basis for the column space of the xcov matrix
+            self.u, _ = torch.linalg.qr(self.xcov)
+        else:
+            # We only want to erase the highest energy part of the subspace
+            self.u, _, _ = torch.svd_lowrank(self.xcov, q=self.rank)
+
+        return self
+
+    @property
+    def P(self) -> Tensor:
+        """Projection matrix for removing the subspace."""
+        eye = torch.eye(self.x_dim, device=self.u.device, dtype=self.u.dtype)
+        return eye - self.u @ self.u.mT
+
+    @property
+    def xcov(self) -> Tensor:
+        """The cross-covariance matrix."""
+        return self.xcov_M2 / self.n
---
+++ tests/test_concept_eraser.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pytest
+import torch
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
+from concept_erasure import ConceptEraser, to_one_hot
+
+
+def test_stats():
+    num_features = 3
+    num_classes = 2
+    batch_size = 10
+    num_batches = 5
+
+    # Initialize the ConceptEraser
+    eraser = ConceptEraser(num_features, num_classes)
+
+    # Generate random data
+    torch.manual_seed(42)
+    x_data = [
+        torch.randn(batch_size, num_features) for _ in range(num_batches)
+    ]
+    y_data = [
+        torch.randint(0, num_classes, (batch_size, num_classes))
+        for _ in range(num_batches)
+    ]
+
+    # Compute cross-covariance matrix using batched updates
+    for x, y in zip(x_data, y_data):
+        eraser.update(x, y)
+
+    # Compute the expected cross-covariance matrix using the whole dataset
+    x_all = torch.cat(x_data)
+    y_all = torch.cat(y_data)
+    mean_x = x_all.mean(dim=0)
+    mean_y = y_all.type_as(x_all).mean(dim=0)
+    x_centered = x_all - mean_x
+    y_centered = y_all - mean_y
+    expected_xcov = torch.einsum("b...m,b...n->...mn", x_centered, y_centered)
+    expected_xcov /= batch_size * num_batches
+
+    # Compare the computed cross-covariance matrix with the expected one
+    torch.testing.assert_close(eraser.xcov, expected_xcov)
+
+
+# Both `1` and `2` are binary classification problems, but `1` means the labels are
+# encoded in a 1D one-hot vector, while `2` means the labels are encoded in an
+# n x 2 one-hot matrix.
+@pytest.mark.parametrize("num_classes", [1, 2, 3, 5, 10, 20])
+def test_projection(num_classes: int):
+    n, d = 2048, 128
+    num_distinct = max(num_classes, 2)
+
+    X, Y = make_classification(
+        n_samples=n,
+        n_features=d,
+        n_classes=num_distinct,
+        n_informative=num_distinct,
+        random_state=42,
+    )
+    X_t = torch.from_numpy(X)
+    Y_t = torch.from_numpy(Y)
+    if num_classes > 1:
+        Y_t = to_one_hot(Y_t, num_classes)
+
+    eraser = ConceptEraser(d, num_classes, dtype=torch.float64).update(X_t, Y_t)
+    X_ = eraser(X_t)
+
+    # Heuristic threshold for singular values taken from torch.linalg.pinv
+    eps = max(n, d) * torch.finfo(X_.dtype).eps
+
+    # Check that the rank of the update is num_classes + 1
+    # The +1 comes from subtracting the mean before projection
+    rank = torch.linalg.svdvals(X_t - X_).gt(eps).sum().float()
+    torch.testing.assert_close(rank, torch.tensor(num_classes + 1.0))
+
+    # Compute class means and check that they are equal after the projection
+    class_means_ = [X_.numpy()[Y == c].mean(axis=0) for c in range(num_distinct)]
+    np.testing.assert_almost_equal(class_means_[1:], class_means_[:-1])
+
+    # Sanity check that class means are NOT equal before the projection
+    class_means = [X[Y == c].mean(axis=0) for c in range(num_distinct)]
+    assert not np.allclose(class_means[1:], class_means[:-1])
+
+    # Logistic regression should not be able to learn anything
+    null_lr = LogisticRegression(max_iter=1000, tol=0.0).fit(X_.numpy(), Y)
+    beta = torch.from_numpy(null_lr.coef_)
+    assert beta.norm(p=torch.inf) < eps
+
+    # Sanity check that it DOES learn something before the projection
+    real_lr = LogisticRegression(max_iter=1000).fit(X, Y)
+    beta = torch.from_numpy(real_lr.coef_)
+    assert beta.norm(p=torch.inf) > 0.1
