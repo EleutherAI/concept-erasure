e1239f654e23bcbf29e140c2827dec3e424a1257
Nora Belrose <belrose.nora@gmail.com>
2023-07-26 07:52:07 +0000

    Oracle LEACE implementation

--- tests/test_leace.py
+++ tests/test_leace.py
@@ -1,5 +1,6 @@
 from itertools import pairwise, product
 
+import numpy as np
 import pytest
 import torch
 import torch.nn.functional as F
@@ -7,7 +8,13 @@ from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
-from concept_erasure import ErasureMethod, LeaceFitter, optimal_linear_shrinkage
+from concept_erasure import (
+    ErasureMethod,
+    LeaceFitter,
+    OracleEraser,
+    OracleFitter,
+    optimal_linear_shrinkage,
+)
 
 
 @pytest.mark.parametrize("shrinkage", [False, True])
@@ -19,19 +26,21 @@ def test_stats(shrinkage: bool):
     N = batch_size * num_batches
 
     fitter = LeaceFitter(num_features, num_classes, shrinkage=shrinkage)
+    oracle = OracleFitter(num_features, num_classes, shrinkage=shrinkage)
 
     # Generate random data
     torch.manual_seed(42)
     x_data = [torch.randn(batch_size, num_features) for _ in range(num_batches)]
-    y_data = [
+    z_data = [
         torch.randint(0, num_classes, (batch_size, num_classes))
         for _ in range(num_batches)
     ]
     projections = []
 
     # Compute cross-covariance matrix using batched updates
-    for x, y in zip(x_data, y_data):
-        fitter.update(x, y)
+    for x, z in zip(x_data, z_data):
+        fitter.update(x, z)
+        oracle.update(x, z)
         projections.append(fitter.eraser.P)
 
     # Make sure the cached eraser is getting invalidated on update() correctly
@@ -40,32 +49,80 @@ def test_stats(shrinkage: bool):
 
     # Compute the expected cross-covariance matrix using the whole dataset
     x_all = torch.cat(x_data)
-    y_all = torch.cat(y_data)
+    z_all = torch.cat(z_data)
     mean_x = x_all.mean(dim=0)
-    mean_y = y_all.type_as(x_all).mean(dim=0)
+    mean_z = z_all.type_as(x_all).mean(dim=0)
     x_centered = x_all - mean_x
-    y_centered = y_all - mean_y
+    z_centered = z_all - mean_z
 
-    expected_cov = torch.einsum("b...m,b...n->...mn", x_centered, x_centered)
+    expected_sigma_xx = torch.einsum("b...m,b...n->...mn", x_centered, x_centered)
+    expected_sigma_zz = torch.einsum("b...m,b...n->...mn", z_centered, z_centered)
     if shrinkage:
-        expected_cov = optimal_linear_shrinkage(
-            expected_cov / N, batch_size * num_batches
+        expected_sigma_xx = optimal_linear_shrinkage(
+            expected_sigma_xx / N, batch_size * num_batches
+        )
+        expected_sigma_zz = optimal_linear_shrinkage(
+            expected_sigma_zz / N, batch_size * num_batches
         )
     else:
-        expected_cov /= N - 1
+        expected_sigma_xx /= N - 1
+        expected_sigma_zz /= N - 1
 
-    expected_sigma_xz = torch.einsum("b...m,b...n->...mn", x_centered, y_centered)
+    expected_sigma_xz = torch.einsum("b...m,b...n->...mn", x_centered, z_centered)
     expected_sigma_xz /= N - 1
 
-    torch.testing.assert_close(fitter.sigma_xx, expected_cov)
+    torch.testing.assert_close(fitter.sigma_xx, expected_sigma_xx)
     torch.testing.assert_close(fitter.sigma_xz, expected_sigma_xz)
+
+    torch.testing.assert_close(oracle.sigma_xz, expected_sigma_xz)
+    torch.testing.assert_close(oracle.sigma_zz, expected_sigma_zz)
+
+
+def check_guardedness(
+    dirty_x: np.ndarray | None, scrubbed_x: np.ndarray, y: np.ndarray, eps: float
+):
+    # Logistic regression should not be able to learn anything.
+    null_lr = LogisticRegression(penalty=None, tol=0.0).fit(
+        # Weirdly, in order for this to work consistently with solver='lbfgs', we
+        # need to center the design matrix first; otherwise the coefficients aren't
+        # quite small enough. Other solvers don't have this problem but they're
+        # slow. In theory, the centroid of X shouldn't matter to the solution.
+        scrubbed_x - scrubbed_x.mean(axis=0),
+        y,
+    )
+    assert abs(null_lr.coef_).max() < eps
+
+    # Linear SVM shouldn't be able to learn anything either
+    null_svm = LinearSVC(
+        # The dual formulation injects random noise into the solution
+        dual=False,
+        # Unfortunately the intercept is subject to L2 regularization; setting this
+        # to a large value largely cancels out the effect. Regularizing the
+        # intercept can cause the coefficients to get larger than they should be.
+        intercept_scaling=1e6,
+        tol=eps,
+    ).fit(scrubbed_x, y)
+    assert abs(null_svm.coef_).max() < eps
+
+    if dirty_x is not None:
+        # Sanity check that it DOES learn something before erasure
+        real_lr = LogisticRegression(penalty=None, tol=0.0).fit(
+            # Do the same centering operation here to be consistent
+            dirty_x - dirty_x.mean(axis=0),
+            y,
+        )
+        assert abs(real_lr.coef_).max() > 0.1
+
+        real_svm = LinearSVC(dual=False, intercept_scaling=1e6, tol=eps).fit(dirty_x, y)
+        assert abs(real_svm.coef_).max() > 0.1
 
 
 # Both `1` and `2` are binary classification problems, but `1` means the labels are
 # encoded in a 1D one-hot vector, while `2` means the labels are encoded in an
 # n x 2 one-hot matrix.
 @pytest.mark.parametrize("num_classes", [1, 2, 3, 5, 10, 20])
-def test_projection(num_classes: int):
+def test_erasure(num_classes: int):
+    eps = 2e-9
     n, d = 2048, 128
     num_distinct = max(num_classes, 2)
 
@@ -83,9 +140,15 @@ def test_projection(num_classes: int):
     else:
         Y_1h = Y_t
 
+    # Benchmark against the theoretical optimal eraser
+    oracle = OracleEraser.fit(X_t, Y_1h)
+    X_oracle = oracle(X_t, Y_1h)
+
+    check_guardedness(None, X_oracle.numpy(), Y.reshape(n, -1), eps)
+    oracle_loss = F.mse_loss(X_oracle, X_t)  # Optimal surgicality
+
     bools = [False, True]
     methods: list[ErasureMethod] = ["leace", "orth"]
-    eps = 2e-9
     for affine, method, shrink in product(bools, methods, bools):
         # Shrinkage not applicable to orthogonal projection
         if method == "orth" and shrink:
@@ -122,6 +185,9 @@ def test_projection(num_classes: int):
         # Should be optimal iff we're using LEACE with the bias term and no shrinkage
         assert (A.grad.norm() < eps) == (affine and method == "leace" and not shrink)
 
+        # Sanity check that we're not beating the oracle
+        assert oracle_loss <= loss
+
         # Check idempotence
         torch.testing.assert_close(eraser(X_), X_)
 
@@ -145,37 +211,4 @@ def test_projection(num_classes: int):
         )
         assert not torch.allclose(class_means[1:], class_means[:-1])
 
-        # Logistic regression should not be able to learn anything.
-        null_lr = LogisticRegression(penalty=None, tol=0.0).fit(
-            # Weirdly, in order for this to work consistently with solver='lbfgs', we
-            # need to center the design matrix first; otherwise the coefficients aren't
-            # quite small enough. Other solvers don't have this problem but they're
-            # slow. In theory, the centroid of X shouldn't matter to the solution.
-            X_.numpy() - X_.numpy().mean(axis=0),
-            Y,
-        )
-        assert abs(null_lr.coef_).max() < eps
-
-        # Sanity check that it DOES learn something before the projection
-        real_lr = LogisticRegression(penalty=None, tol=0.0).fit(
-            # Do the same centering operation here to be consistent
-            X - X.mean(axis=0),
-            Y,
-        )
-        assert abs(real_lr.coef_).max() > 0.1
-
-        # Linear SVM shouldn't be able to learn anything either
-        null_svm = LinearSVC(
-            # The dual formulation injects random noise into the solution
-            dual=False,
-            # Unfortunately the intercept is subject to L2 regularization; setting this
-            # to a large value largely cancels out the effect. Regularizing the
-            # intercept can cause the coefficients to get larger than they should be.
-            intercept_scaling=1e6,
-            tol=eps,
-        ).fit(X_.numpy(), Y)
-        assert abs(null_svm.coef_).max() < eps
-
-        # But it should learn something before the projection
-        real_svm = LinearSVC(dual=False, intercept_scaling=1e6, tol=eps).fit(X, Y)
-        assert abs(real_svm.coef_).max() > 0.1
+        check_guardedness(X, X_.numpy(), Y, eps)
